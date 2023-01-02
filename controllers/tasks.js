3f608715b429147a977e79fcb2c1b167ed5151dd
Oussama <oussamalkd99@gmail.com>
2023-01-02 22:23:35 +0100

    feat: Tasks controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,6 +2,26 @@ const getAllTasks = (req, res) => {
     res.send("Gett all tasks")
 }
 
+const createTask = (req, res) => {
+    res.send("create Task")
+}
+
+const getTask = (req, res) => {
+    res.send("single Task")
+}
+
+const updateTask = (req, res) => {
+    res.send("Update Task")
+}
+
+const deleteTask = (req, res) => {
+    res.send("Delete Task")
+}
+
 module.exports = {
-    getAllTasks
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
 }
