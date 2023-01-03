6a0c987d9343426ca7148a26e621d8cff90a6d64
Oussama <oussamalkd99@gmail.com>
2023-01-03 15:53:46 +0100

    feat: setup dotenv

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 2405
 const tasksRoutes = require("./routes/tasks")
 const connectDB = require("./db/connect")
+require("dotenv").config()
 
 //middleware
 app.use(express.json())
@@ -11,10 +12,10 @@ app.use(express.json())
 app.use(express.static("./public"))
 app.use("/api/v1/tasks", tasksRoutes)
 
-
+// connet to Database
 const start = async() => {
     try {
-        await connectDB()
+        await connectDB(process.env.MONGO_URI)
         //listen
         app.listen(port,console.log(`App is runing on http://localhost:${port}`))
     }
@@ -24,4 +25,3 @@ const start = async() => {
 }
 
 start()
-
--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose")
 
-const connectionString = "mongodb+srv://oussamaLkdTaskManager:TaskManager24052023@expressprojects.lhnbjxr.mongodb.net/task-manager?retryWrites=true&w=majority"
-
-
 const connectDB = (url) => {
     mongoose.set('strictQuery',false);
     mongoose
-        .connect(connectionString, {
+        .connect(url, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
