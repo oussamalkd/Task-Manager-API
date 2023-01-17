4eacc0c95844de35dcfea5369668d9f39d9a7d36
Oussama Louelkadi <oussamalkd@pop-os.localdomain>
2023-01-17 12:00:58 +0100

    feat: 404 middleware

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const port = 2405
 const tasksRoutes = require("./routes/tasks")
 const connectDB = require("./db/connect")
 require("dotenv").config()
+const notFound = require("./middleware/not-found")
 
 //middleware
 app.use(express.json())
+app.use(express.static("./public"))
 
 //routes
-app.use(express.static("./public"))
 app.use("/api/v1/tasks", tasksRoutes)
+app.use(notFound)
 
 // connet to Database
 const start = async() => {
