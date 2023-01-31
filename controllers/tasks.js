053b3c78344d7004732e4de400adcf4fd1325681
oussamalkd <oussamalkd99@gmail.com>
2023-01-31 14:54:08 +0100

    feat: custom error class

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,7 @@
 const { json } = require("express");
 const Task = require("../models/tasks");
 const asyncWrraper = require("../middleware/asyncWrapper")
+const { createCustomError } = require("../error/custom-error")
 
 const getAllTasks = asyncWrraper( async (req, res) => {
     const tasks = await Task.find({})
@@ -12,28 +13,27 @@ const createTask = asyncWrraper( async (req, res) => {
     res.status(201).json({success: true, data: task})
 })
 
-const getTask = asyncWrraper( async (req, res) => {
+const getTask = asyncWrraper( async (req, res, next) => {
     const task = await Task.findOne({_id: req.params.id})
     if(!task) {
-        return res.status(404).json({msg: "The Task that you're looking for is removed or doesn't exist"})
+        return next(createCustomError("The Task that you're looking for is removed or doesn't exist", 404))
     }
     res.status(200).json({success: true, data: task})
 })
 
-const updateTask = asyncWrraper( async (req, res) => {
-    
+const updateTask = asyncWrraper( async (req, res, next) => {
     const task = await Task.findByIdAndUpdate(
         { _id: req.params.id}, req.body,{ new: true,  runValidators: true })
     if(!task) {
-        return res.status(404).json({msg: "The Task that you're looking for is removed or doesn't exist"})
+        return next(createCustomError("The Task that you're looking for is removed or doesn't exist", 404))
     }
     res.status(200).json({success: true, data: task})
 })
 
-const deleteTask = asyncWrraper( async (req, res) => {
+const deleteTask = asyncWrraper( async (req, res, next) => {
     const task = await Task.findOneAndDelete({_id: req.params.id})
     if(!task) {
-        return res.status(404).json({ success: false, msg: "task not found"})
+        return next(createCustomError("task not found", 404))
     }
     res.status(200).json({ success: true, msg: "Task deleted successfuly"})
 })
--- middleware/errorHandller.js
+++ middleware/errorHandller.js
@@ -1,4 +1,9 @@
+const { CustomAPIError } = require("../error/custom-error")
+
 const erroHandllerMiddleware = (err, req, res, next) => {
+    if (err instanceof CustomAPIError) {
+        return res.status(err.statusCode).json({ msg: err.message })
+    }
     return res.status(500).json({ msg: "Something went wrong, please try again later!"})
 }
 
